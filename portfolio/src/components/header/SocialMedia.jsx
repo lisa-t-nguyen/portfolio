9bc1c1aed259623ab899234c424d3f599a7020ae
Lisa Nguyen <lisanguyensc@gmail.com>
2023-04-12 19:33:05 -0400

    Added Social Media component and react icons

---
+++ portfolio/src/components/header/SocialMedia.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+
+const SocialMedia = () => {
+  return (
+    <div className='SocialMedia'>
+        <a href='https://www.linkedin.com/in/lisa--nguyen/' target='_blank'><BsLinkedin/></a>
+        <a href='https://github.com/lisa-t-nguyen' target='_blank'></a>
+    </div>
+  )
+}
+
+export default SocialMedia
--- portfolio/src/components/header/header.jsx
+++ portfolio/src/components/header/header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import HeaderButtons from './HeaderButtons'
+import Me from '../../assets/Me.png'
+import SocialMedia from './SocialMedia'
 
 const header = () => {
   return (
@@ -10,6 +12,13 @@ const header = () => {
         <h1>Lisa Nguyen</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <HeaderButtons />
+        <SocialMedia />
+
+        <div className="me">
+          <img src={Me} alt='Lisa Nguyen' />
+        </div>
+
+        <a href='#contact' className='scroll_down'>Scroll Down</a>
       </div>
     </header>
   )
