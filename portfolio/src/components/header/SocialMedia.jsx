afed11278f7862e691e00ab91fc11b8ef0f51d5c
Lisa Nguyen <lisanguyensc@gmail.com>
2023-04-12 22:55:10 -0400

    Finished header component with icons and image

--- portfolio/src/components/header/SocialMedia.jsx
+++ portfolio/src/components/header/SocialMedia.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
 
 const SocialMedia = () => {
   return (
     <div className='SocialMedia'>
         <a href='https://www.linkedin.com/in/lisa--nguyen/' target='_blank'><BsLinkedin/></a>
-        <a href='https://github.com/lisa-t-nguyen' target='_blank'></a>
+        <a href='https://github.com/lisa-t-nguyen' target='_blank'><BsGithub/></a>
     </div>
   )
 }
